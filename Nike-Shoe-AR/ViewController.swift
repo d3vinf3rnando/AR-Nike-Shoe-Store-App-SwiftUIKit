4caecdbf520f2a2124b19dfb487fa2898b1bb4e3
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-12-25 20:01:22 +0530

    Initial Commit

---
+++ Nike-Shoe-AR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Nike-Shoe-AR
+//
+//  Created by Devin Fernando on 2022-12-25.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
---
+++ Nike-Shoe-ARUITests/Nike_Shoe_ARUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Nike_Shoe_ARUITestsLaunchTests.swift
+//  Nike-Shoe-ARUITests
+//
+//  Created by Devin Fernando on 2022-12-25.
+//
+
+import XCTest
+
+final class Nike_Shoe_ARUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
